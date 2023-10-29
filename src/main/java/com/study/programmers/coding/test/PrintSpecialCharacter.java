06fb724ec5f4229b1b977075a99025bfb67b95e0
pej4303 <pej4303@gmail.com>
2023-10-30 02:13:52 +0900

    동기화

---
+++ src/main/java/com/study/programmers/coding/test/PrintSpecialCharacter.java
@@ -0,0 +1,12 @@
+package com.study.programmers.coding.test;
+
+/**
+ * 아래와 같이 출력되게 하세요.
+ * !@#$%^&*(\'"<>?:;
+ */
+public class PrintSpecialCharacter {
+    public static void main(String[] args) {
+        String str = "!@#$%^&*(\\'\"<>?:;";
+        System.out.println(str);
+    }
+}
--- src/main/java/com/study/programmers/coding/test/RepeatStr.java
+++ src/main/java/com/study/programmers/coding/test/RepeatStr.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 /**
  * 문자열 str과 정수 n이 주어집니다.
  * str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+ *
+ * @date 2023-10-30
  */
 public class RepeatStr {
     /**
