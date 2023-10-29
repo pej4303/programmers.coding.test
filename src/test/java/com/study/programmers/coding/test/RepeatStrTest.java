c48e31eec3adea2534509547e1ae36c0992c720f
pej4303 <pej4303@gmail.com>
2023-10-30 01:07:26 +0900

    동기화

---
+++ src/test/java/com/study/programmers/coding/test/RepeatStrTest.java
@@ -0,0 +1,14 @@
+package com.study.programmers.coding.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepeatStrTest {
+
+    @Test
+    void main() {
+        String str = "string";
+        System.out.println(str.repeat(10));
+    }
+}
