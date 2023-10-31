10fe2a70d26ff6cf64582652afd41d482d17a8b7
ejpark <pej4303@gmail.com>
2023-10-31 23:10:56 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        /**
+         * 제한사항
+         * 1 ≤ my_string의 길이 ≤ 100
+         * my_string은 영소문자로만 이루어져 있습니다.
+         * 1 ≤ k ≤ 100
+         */
+        
+        
+        return my_string.repeat(k);
+    }
+}
