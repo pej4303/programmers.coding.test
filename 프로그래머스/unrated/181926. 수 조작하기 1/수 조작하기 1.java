f3b705805d754d524ea220d7ef66d4ea177968f0
ejpark <pej4303@gmail.com>
2023-11-04 00:50:06 +0900

    [level unrated] Title: 수 조작하기 1, Time: 20.35 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        
+        for (String str : control.split("")) {
+            switch(str) {
+                case "w":
+                    answer += 1;
+                    break;
+                case "s":
+                    answer -= 1;
+                    break;
+                case "d":
+                    answer += 10;
+                    break;
+                case "a":
+                    answer -= 10;
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
