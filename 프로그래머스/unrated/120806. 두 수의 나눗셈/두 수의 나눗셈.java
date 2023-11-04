1b76704caa95c863f019ad2444f64cb2b116cd46
ejpark <pej4303@gmail.com>
2023-11-04 22:07:16 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        if ( (0<= num1 && num1 <= 100) && (0<= num2 && num2 <= 100) ) {
+           answer = (int) (num1 / (double)num2 * 1000);
+        }
+        
+        return answer;
+    }
+}
