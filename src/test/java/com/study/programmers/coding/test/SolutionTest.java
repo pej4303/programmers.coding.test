cfd3898deb7ca5c3e67dc66e027741c2b1e1bf63
pej4303 <pej4303@gmail.com>
2024-03-10 16:12:47 +0900

    동기화

--- src/test/java/com/study/programmers/coding/test/SolutionTest.java
+++ src/test/java/com/study/programmers/coding/test/SolutionTest.java
@@ -1,29 +1,56 @@
 package com.study.programmers.coding.test;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
 
     @Test
     void test1() {
-        int[] numbers = {3, 4};
-        int[] our_score = {85, 93};
-        int[] score_list = {85, 92, 38, 93, 48, 85, 92, 56};
-        int num_student = numbers.length;
-        String[] answer = new String[num_student];
+        String[] storage = {"pencil", "pencil", "pencil", "book"};
+        int[] num = {2, 4, 3, 1};
 
-        for (int i = 0; i < num_student; i++) {
-            System.out.println(numbers[i]);
+        String result = "pencil";
 
-            if (our_score[numbers[i]] == score_list[numbers[i]]) {
-                answer[numbers[i]] = "Same";
-            } else {
-                answer[numbers[i]] = "Different";
+        int num_item = 0;
+        String[] clean_storage = new String[storage.length];
+        int[] clean_num = new int[num.length];
+
+        for(int i=0; i<storage.length; i++){
+            int clean_idx = -1;
+            for(int j=0; j<num_item; j++){
+                if(storage[i].equals(clean_storage[j])){
+                    clean_idx = j;
+                    break;
+                }
+            }
+            if(clean_idx == -1){
+                clean_storage[num_item] = storage[num[i]];
+                clean_num[num_item] = num[i];
+                num_item += 1;
+            }
+            else{
+                clean_num[clean_idx] += num[i];
             }
         }
-        System.out.println(Arrays.toString(answer));
+
+        System.out.println(Arrays.toString(clean_storage));
+        System.out.println(Arrays.toString(clean_num));
+
+        // 아래 코드에는 틀린 부분이 없습니다.
+        int num_max = -1;
+        String answer = "";
+        for(int i=0; i<num_item; i++){
+            if(clean_num[i] > num_max){
+                num_max = clean_num[i];
+                answer = clean_storage[i];
+            }
+        }
+
+        assertEquals(answer, result);
     }
 }
-
