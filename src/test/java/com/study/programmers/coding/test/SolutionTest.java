a7702b8fece925219ada5e4ac295cdfe4607986a
pej4303 <pej4303@gmail.com>
2024-03-10 15:28:16 +0900

    동기화

--- src/test/java/com/study/programmers/coding/test/SolutionTest.java
+++ src/test/java/com/study/programmers/coding/test/SolutionTest.java
@@ -8,8 +8,22 @@ public class SolutionTest {
 
     @Test
     void test1() {
-//        System.out.println(LocalDate.now().getYear());
-        System.out.println(new Date().getYear());
+        int[] numbers = {3, 4};
+        int[] our_score = {85, 93};
+        int[] score_list = {85, 92, 38, 93, 48, 85, 92, 56};
+        int num_student = numbers.length;
+        String[] answer = new String[num_student];
+
+        for (int i = 0; i < num_student; i++) {
+            System.out.println(numbers[i]);
+
+            if (our_score[numbers[i]] == score_list[numbers[i]]) {
+                answer[numbers[i]] = "Same";
+            } else {
+                answer[numbers[i]] = "Different";
+            }
+        }
+        System.out.println(Arrays.toString(answer));
     }
 }
 
