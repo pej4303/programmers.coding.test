23af518f8ea84b33aaafc651675c10d7f205a770
ejpark <pej4303@gmail.com>
2024-04-16 22:18:22 +0900

    [level 2] Title: 올바른 괄호, Time: 16.74 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        if (s.length() > 100000) {
+            return false;
+        }
+        
+        /*Stack<String> stack = new Stack<>();
+        for (String str : s.split("")) {
+            if ("(".equals(str)) {
+                stack.push(str); // 요소 넣기
+            } else if (")".equals(str)) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop(); // 맨 위에 있는 요소를 제거
+            }
+        }*/
+        
+        Stack<Character> stack = new Stack<>();  
+        for (char c : s.toCharArray()) {  
+            if (c == '(') {  
+                stack.push('(');  
+            } else if (c == ')') {  
+                if (stack.isEmpty()) {  
+                    return false;  
+                }  
+                stack.pop();  
+            }  
+        }  
+        // () 한쌍으로 올바른 괄호라면 개수가 0이 나옴
+        // System.out.println("개수 : " + stack.size());
+        
+        // answer = stack.isEmpty();
+
+        return stack.isEmpty();
+    }
+}
