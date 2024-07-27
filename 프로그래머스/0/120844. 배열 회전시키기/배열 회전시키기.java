8b6ea1c714f5d69f9ace5668df7cde51790ff533
ejpark <pej4303@gmail.com>
2024-07-27 22:27:53 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -21,6 +21,14 @@ class Solution {
         // right => 원본 배열의 마지막이 맨 앞으로
         // left => 원본 배열의 첫번째가 맨 뒤로
         
+        /*
+        Object src  : 복사할 객체, 원본 배열
+        int srcPos : 복사 시작할 index
+        Object dest : 복사된 배열
+        int destPos : 원본으로부터 가져온 데이터를 복사된 배열의 몇 번째부터 추가할지
+        int length : 카피되는 배열 요소 개수
+        */
+        
         // 원본 배열을 복사해서 특정위치에 넣기
         System.arraycopy(numbers, copyIndex, answer, startIndex, numbers.length-1);
         // System.out.println("answer = " + Arrays.toString(answer));
