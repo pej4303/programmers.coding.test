d80dcbf37c6c72e060bcda355a60df25380f3278
ejpark <pej4303@gmail.com>
2024-08-12 18:21:34 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.13 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/README.md
+++ 프로그래머스/0/120902. 문자열 계산하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 계산하기 - 120902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902) 
 
 ### 성능 요약
 
-메모리: 68.7 MB, 시간: 0.13 ms
+메모리: 75 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 12일 18:19:50
+2024년 08월 12일 18:21:40
 
 ### 문제 설명
 
--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -39,14 +39,14 @@ class Solution {
         String[] res = my_string.split(" ");
         answer = Integer.parseInt(res[0]); // 첫번째 값 저장.
         
-        for(int i = 1; i < res.length; i++){
-            if(i%2!=0){
-                if(res[i].equals("+")){
+        for (int i = 1; i < res.length; i++) {
+            if (i%2!=0) {
+                if (res[i].equals("+")) { 
                     answer += Integer.parseInt(res[i+1]);
                 } else {
                     answer -= Integer.parseInt(res[i+1]);
                 }    
-            }
+           }
         }
 
         return answer;
