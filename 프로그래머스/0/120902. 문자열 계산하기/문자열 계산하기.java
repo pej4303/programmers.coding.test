ae5b92451c45c3a72447f0bfe6b405160113e66e
ejpark <pej4303@gmail.com>
2024-08-12 18:19:44 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.13 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,54 @@
+/*class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        boolean isFlag = false;
+        char operator = '+';
+
+        // 유효성 검사
+        if (my_string.length() < 5 || my_string.length() > 100) {
+            return answer;
+        }
+
+        for (char c : my_string.toCharArray()) {
+            if (c == ' ') { 
+                continue;
+            } else if (c == '+' || c == '-') {
+                operator = c;
+                isFlag = true;
+            } else if (Character.isDigit(c)) {
+                int num = Character.getNumericValue(c);
+                if (isFlag) {
+                    if (operator == '+') {
+                        answer += num;
+                    } else if (operator == '-') {
+                        answer -= num;
+                    }
+                    isFlag = false;
+                } else {
+                    answer = num;
+                }
+            }
+            // System.out.println(answer);
+        }
+        return answer;
+    }
+}*/
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] res = my_string.split(" ");
+        answer = Integer.parseInt(res[0]); // 첫번째 값 저장.
+        
+        for(int i = 1; i < res.length; i++){
+            if(i%2!=0){
+                if(res[i].equals("+")){
+                    answer += Integer.parseInt(res[i+1]);
+                } else {
+                    answer -= Integer.parseInt(res[i+1]);
+                }    
+            }
+        }
+
+        return answer;
+    }
+}
