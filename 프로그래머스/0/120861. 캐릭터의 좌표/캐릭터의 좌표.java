321e0b18a335e5dc39694316525f0f5aa8ca46bd
ejpark <pej4303@gmail.com>
2024-08-15 21:05:11 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] position = {0 , 0};
+        
+        // 유효성검사
+        /*if ( 1 > board[0] || 99 > board[0] ) {
+            return position;
+        }
+        if ( 1 > board[1] || 99 > board[1] ) {
+            return position;
+        }*/
+        
+        
+        int maxRight = board[0] / 2;
+        int minLeft = (board[0] / 2) * -1;
+        int minDown = (board[1] / 2) * -1;
+        int maxUp = board[1] / 2;
+        
+        for (String str : keyinput) {
+            switch (str) {
+                case "up" :
+                    position[1] += 1;
+                    if (maxUp < position[1]) {
+                        position[1] = maxUp;
+                    }
+                    break;
+                case "down" :
+                    position[1] -= 1;
+                    if (minDown > position[1]) {
+                        position[1]= minDown;
+                    }
+                    break;
+                case "left" :
+                    position[0] -= 1;
+                    if (minLeft > position[0]) {
+                        position[0] = minLeft;
+                    }
+                    break;
+                case "right" :
+                    position[0] += 1;
+                    if (maxRight < position[0]) {
+                        position[0] = maxRight;
+                    }
+                    break;
+            }
+        }
+        
+        // System.out.println( Arrays.toString(position) );
+        
+        return position;
+    }
+}
