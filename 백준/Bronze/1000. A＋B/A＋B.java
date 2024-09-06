cef04fae714224f45a5b96086bc6df17149dd4b1
ejpark <pej4303@gmail.com>
2024-09-07 00:04:05 +0900

    [Bronze V] Title: A+B, Time: 164 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();  // 첫 번째 숫자
+        int num2 = sc.nextInt();  // 두 번째 숫자
+        
+        System.out.println(num1 + num2);
+    }
+}
