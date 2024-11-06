e02586a442782d91dddf78a86ec89f89520bf226
ejpark <pej4303@gmail.com>
2024-11-06 12:10:44 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.28 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int len = arr1.length;
+        
+        int[][] answer =  new int[len][arr1[0].length];
+        
+        for (int i=0; i<len; i++) {
+            for (int j=0; j<arr1[i].length; j++) {
+                //System.out.println(arr1[i][j]);
+                //System.out.println(arr2[i][j]);
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+                // System.out.println(arr2[i][j]);
+            }   
+        }
+        
+        return answer;
+    }
+}
