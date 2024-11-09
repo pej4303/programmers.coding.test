79cc8cbde0ebcc8b4abf7ebefc6f06de1c14df63
ejpark <pej4303@gmail.com>
2024-11-09 19:29:19 +0900

    [Bronze V] Title: We love kriii, Time: 100 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "강한친구 대한육군";
+        System.out.println(str);
+        System.out.println(str);
+    }    
+}
