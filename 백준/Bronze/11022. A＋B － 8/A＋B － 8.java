9de814293cf66613d39b69b173bab220864bbf3a
ejpark <pej4303@gmail.com>
2024-11-12 14:41:26 +0900

    [Bronze V] Title: A+B - 8, Time: 216 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i=1; i<=t; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            System.out.println(String.format("Case #%d: %d + %d = %d", i, x, y, x+y));
+        }
+    }
+}
