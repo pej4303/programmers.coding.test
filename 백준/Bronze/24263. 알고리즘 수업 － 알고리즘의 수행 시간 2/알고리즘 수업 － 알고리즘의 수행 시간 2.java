fbc2cbbf2c66d748a19a4de2f0c5618f07ec28dc
ejpark <pej4303@gmail.com>
2024-11-23 01:04:03 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        System.out.println(n);
+        // 시간복잡도 O(N) => 1
+        // 1차원 배열은 항상 1이 나옴
+        System.out.println(1);
+    }
+}
