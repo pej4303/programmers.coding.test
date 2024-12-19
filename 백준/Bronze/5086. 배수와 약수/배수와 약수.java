85a3cac67c1b11bb330f1a3bc852d36148e4a1d8
ejpark <pej4303@gmail.com>
2024-12-20 00:17:36 +0900

    [Bronze III] Title: 배수와 약수, Time: 164 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if ( x == 0 && y == 0 ) {
+                break;
+            }
+
+            if ( x < y && y % x == 0) {
+                System.out.println("factor");
+            } else if ( x > y && x % y == 0 ) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
