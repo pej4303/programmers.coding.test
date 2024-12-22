c7e66a5825f396dcb51301270e52fa8c354c05ba
ejpark <pej4303@gmail.com>
2024-12-22 16:46:38 +0900

    [Bronze III] Title: 최댓값, Time: 188 ms, Memory: 17844 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -3,17 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] arr = new int[9][9];
-
         int max = Integer.MIN_VALUE;
         int max_i = 0;
         int max_j = 0;
 
         for (int i=0; i<9; i++) {
             for (int j=0; j<9; j++) {
-                arr[i][j] = sc.nextInt();
-                if (arr[i][j] > max) {
-                    max = arr[i][j];
+                int tmp = sc.nextInt();
+                if (tmp > max) {
+                    max = tmp;
                     max_i = i;
                     max_j = j;
                 }
