17e6e6afbc7d2b8851fecb7a386158d4087d8805
ejpark <pej4303@gmail.com>
2025-01-06 19:48:54 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int pre1 = 0;   // 이이전값
+        int pre2 = 1;   // 이전값
+        int result = 0; // 결과값
+
+        if (n == 0) {
+            System.out.println(0);
+        } else if (n == 1){
+            System.out.println(1);
+        } else {
+            int i = 1;
+            while (i<n) {
+                result = pre1 + pre2; // 피보나치 수의 합
+//                System.out.println(result);
+                pre1 = pre2;
+                pre2 = result;
+
+                i++;
+            }
+
+            // 출력
+            System.out.println(result);
+        }
+    }
+    // 0  1  2  3  4  5  6  7   8   9   10
+    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+}
