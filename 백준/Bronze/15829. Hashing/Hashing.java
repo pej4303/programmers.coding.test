01a7995706e298479b49903acfb40b2d5085c6e9
ejpark <pej4303@gmail.com>
2025-01-19 17:53:47 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14288 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,20 +1,30 @@
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
+        int r = 31;
+        int m = 1234567891;
         // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         String input = br.readLine();
 
         char[] arr = input.toCharArray();
-        int sum = 0;
-        
-        for (int i=0; i<arr.length; i++) {
+        // int로 하면 범위 초과함 long으로 변경
+        long sum = 0;
+        for (int i=0; i<L; i++) {
             // a -> 1, b->2로 나옴
             int num = arr[i] - 'a' + 1;
-            // 1 * 31^0
-            int tmpNum = num * (int) Math.pow(31, i);
+
+            long a = 1;
+            for (int j=0; j<i; j++) {
+                // 곱하고 바로 mod 연산을 해도 결과값이 같음
+                a = (a * r) % m;
+            }
+            // long 범위 넘지 않게 하기 위해서 mod 연산을 미리 수행함
+            long tmpNum = (num * a) % m;
             sum += tmpNum;
+            // long 범위 넘지 않게 하기 위해서 mod 연산을 미리 수행함
+            sum %= m;
         }
 
         System.out.println(sum);
