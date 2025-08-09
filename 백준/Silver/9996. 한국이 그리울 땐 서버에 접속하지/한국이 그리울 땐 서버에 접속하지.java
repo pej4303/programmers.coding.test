e6c94308127727770fc0738e5d96d629cc8c68ba
ejpark <pej4303@gmail.com>
2025-08-09 20:30:00 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -1,13 +1,17 @@
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws Exception {
+        // 1. 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String pattern = br.readLine().replace("*", ".*");
-        for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            if (input.matches(pattern)) {
+        String[] arr = br.readLine().split("\\*");
+
+        // 2. 로직
+        for (int i=0; i<n; i++) {
+            String text = br.readLine();
+            // 3. startsWith와 endsWith로 앞부분, 뒷부분 비교
+            if ( text.length() >= arr[0].length() + arr[1].length() &&
+                    text.startsWith(arr[0]) && text.endsWith(arr[1]) ) {
                 System.out.println("DA");
             } else {
                 System.out.println("NE");
